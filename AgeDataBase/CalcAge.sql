0acff5d6fc8d31dd18edfe85f99ff01e010303d7
Rafes <rafadvcoliveira@gmail.com>
2022-05-24 12:01:46 -0300

    Fix CalcAge

--- AgeDataBase/CalcAge.sql
+++ AgeDataBase/CalcAge.sql
@@ -1,19 +1,11 @@
 ﻿USE AgeDataBase
 GO
-CREATE TABLE birth_tbl
-(
-	[birth] VARCHAR(11) NOT NULL
-    
-)
-GO
 
 DECLARE		@nasc AS VARCHAR(11) = '17/07/2008',
 			@today AS DATE = GETDATE();
---INSERT INTO birth_tbl.birth
--- VALUES (@nasc);
+INSERT INTO age_tbl (birth)
+VALUES (@nasc);
  
- -- @nasc ou birth?
-SELECT FLOOR(DATEDIFF(DAY, CONVERT(DATE, @nasc), @today) / 365.23076923074) as idade
-FROM birth_tbl
--- todo tentar criar a tablea age_db
+SELECT FLOOR(DATEDIFF(DAY, CONVERT(DATE, @nasc, 103), @today) / 365.23076923074) as idade
+FROM age_tbl
 GO
